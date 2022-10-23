a490fec9e3b6552fb8413033aeba27fc23713680
Pauline Kedogo <paulinerumama@gmail.com>
2022-10-23 22:46:15 +0300

    delete

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-// Script to print 3 lines of text
-
-const myText = 'C is fun\nPython is cool\nJavaScript is amazing';
-console.log(myText);
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
 
